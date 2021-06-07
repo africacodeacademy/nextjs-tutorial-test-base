3e404d86ddc52b67a1395dcb33d9c0ec9860b831
Bonnie Schulkin <biz@bonnieschulkin.com>
2021-06-07 11:35:30 -0700

    Update test to search for actual text

--- pages-tests/index.test.jsx
+++ pages-tests/index.test.jsx
@@ -1,7 +1,11 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 import Home from "../pages/index";
 
 test("App renders user name", () => {
   render(<Home />);
+  const welcomeMessage = screen.getByRole("heading", {
+    name: /Welcome to Next.js/,
+  });
+  expect(welcomeMessage).toBeInTheDocument();
 });
