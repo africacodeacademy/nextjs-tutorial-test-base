2be30968a51b3001043aabb1da7bbc4464391204
Bonnie Schulkin <biz@bonnieschulkin.com>
2021-06-07 11:12:13 -0700

    Add necessary files for Jest, plus .eslintrc.json

---
+++ __mocks__/mockData.js
@@ -0,0 +1,8 @@
+export const mockPostData = {
+  id: "pre-rendering",
+  title: "Two Forms of Pre-rendering",
+  date: "2021-08-01",
+  contentHtml: "<h1>I am a title</h1><p>I am some content</p>",
+};
+
+export const mockAllPostsData = [mockPostData];
