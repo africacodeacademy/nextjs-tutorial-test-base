f54867e9c20f2be826ae119b2144e619935a10e5
Bonnie Schulkin <biz@bonnieschulkin.com>
2021-06-07 11:44:50 -0700

    Update test name

--- pages-tests/index.test.jsx
+++ pages-tests/index.test.jsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 
 import Home from "../pages/index";
 
-test("App renders user name", () => {
+test("App renders welcome message", () => {
   render(<Home />);
   const welcomeMessage = screen.getByRole("heading", {
     name: /Welcome to Next.js/,
